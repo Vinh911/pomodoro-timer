92f526df7304e3f2a44a01b9f6c3b37f5b2c6636
vnguyen <vnguyen@queo-group.com>
2022-12-09 17:32:47 +0100

    add navigation bar and basic routing

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends AbstractController
+{
+    public function index(): Response {
+        return $this->render('timer/timer.html.twig', []);
+    }
+}
